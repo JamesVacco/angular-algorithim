5595fd494aa9832b53e0062ff8c25328c8d45ea6
JamesVacco <gameteccch@gmail.com>
2019-04-06 17:12:55 -0400

    1 file modified

--- src/app/algorithim.ts
+++ src/app/algorithim.ts
@@ -1,5 +1,8 @@
 //h=happiness, a= anger, s= sadness, f=fear
-function algorithim(h,a,s,f){
+
+var emotion
+
+function getEmotion(h,a,s,f){
   if(h >70){
     if(a>70){
       if(s>70){
@@ -49,9 +52,81 @@ function algorithim(h,a,s,f){
     if(f>70){
       return 8
     }
+    else{
+      return 9
+    }
+  }
+  else if(f>70){
+    return 10
+  }
+  else if((h<30)&&(a<30)&&(s<30)&&(f<30)){
+    return 11
+  }
+  else{
+    return 0
   }
 }
 
+function switchStatement(){
+switch(getEmotion(userh,usera,users,userf)) { 
+   case 0: { 
+      emotion = 'unknown'
+      break; 
+   } 
+   case 1: { 
+      //statements; 
+      break; 
+   }
+   case 2: { 
+      emotion = 'unknown'
+      break; 
+   }
+   case 3: { 
+      emotion = 'unknown'
+      break; 
+   }
+   case 4: { 
+      emotion = 'unknown'
+      break; 
+   }
+   case 5: { 
+      emotion = 'unknown'
+      break; 
+   }
+   case 6: { 
+      emotion = 'unknown'
+      break; 
+   } 
+   case 0: { 
+      emotion = 'unknown'
+      break; 
+   } 
+   case 0: { 
+      emotion = 'unknown'
+      break; 
+   } 
+   case 0: { 
+      emotion = 'unknown'
+      break; 
+   } 
+   case 0: { 
+      emotion = 'unknown'
+      break; 
+   } 
+   case 0: { 
+      emotion = 'unknown'
+      break; 
+   } 
+   case 0: { 
+      emotion = 'unknown'
+      break; 
+   } 
+   default: { 
+      //statements; 
+      break; 
+   } 
+} 
+}
 emotion = {1:'Overwhlemed', 2: 'confused', 3:'confused', 4:'happy', 5: 'despair', 6:'bitter',7:'angry',8: 'anxious', 9: 'sad', 10:'afraid', 11:'depressed',12: 'worried', 0: 'unknown'}
 
 userEmo = getEmotion(happy, angry, sad, fearful)
